2907970744f5e19c31bd8b7837b327a1445746a7
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2022-12-12 21:24:11 +0100

    More data added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ const searchCity = document.querySelector('.searchCity')
 const searchInput = document.querySelector('.searchInput')
 const cityInfo = document.querySelector('.cityInfo')
 const temperatureInfo = document.querySelector('.temperatureInfo')
+const feelsLikeInfo = document.querySelector('.feelsLike')
+const minMaxTemp = document.querySelector('.minMaxTemp')
+const humidityInfo = document.querySelector('.humidity')
+const windSpeedInfo = document.querySelector('.windSpeed')
+const sunriseInfo = document.querySelector('.sunrise')
+const sunsetInfo = document.querySelector('.sunset')
 const apikey = "84a9e3473035153f383f2976491a4b4b"
 
 //Weather by location
@@ -48,6 +54,39 @@ function weatherInfo(data) {
 
   const {temp} = data.main
   temperatureInfo.innerHTML = temp + "°C | " + Math.round(((temp * 1.8)+32)*100)/100 + "°F"
+  
+  const {feels_like} = data.main
+  feelsLikeInfo.innerHTML = "Feels like " + feels_like + "°C"
+
+  const {temp_min} = data.main
+  const {temp_max} = data.main
+  minMaxTemp.innerHTML = "Min & max temp: " + temp_min + "°C | " + temp_max + "°C"
+
+  const {humidity} = data.main
+  humidityInfo.innerHTML = "Humidity: "+ humidity + "%"
+
+  const {speed} = data.wind
+  windSpeedInfo.innerHTML = "Wind speed: " + Math.round((speed / 3.6)*100)/100 + " m/s"
+
+  const {sunrise} = data.sys
+  const {sunset} = data.sys
+  const {timezone} = data
+
+  let unix_timestamp_sunrise = (sunrise + timezone - 3600)
+  var date = new Date(unix_timestamp_sunrise * 1000)
+  var hours = date.getHours()
+  var minutes = "0" + date.getMinutes()
+  var formattedTimeSunrise = "0" + hours + ':' + minutes.substr(-2)
+
+  sunriseInfo.innerHTML = "Sunrise: " + formattedTimeSunrise
+
+  let unix_timestamp_sunset = (sunset + timezone - 3600)
+  var date = new Date(unix_timestamp_sunset * 1000);
+  var hours = date.getHours();
+  var minutes = "0" + date.getMinutes();
+  var formattedTimeSunset = hours + ':' + minutes.substr(-2)
+
+  sunsetInfo.innerHTML = "Sunset: " + formattedTimeSunset
 }
 
 //Remember latest location on reload
